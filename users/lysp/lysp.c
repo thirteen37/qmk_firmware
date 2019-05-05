eaca92f4e6c5f840b77a5447ab2b9f7a47a48d77
Yu-Xi Lim <limyuxi@gmail.com>
2019-05-05 15:36:42 +0800

    Layout and cleanup for gaming

--- users/lysp/lysp.c
+++ users/lysp/lysp.c
@@ -8,20 +8,20 @@
 #define FNKY 1 // Fn and media
 #define MSNM 2 // Mouse and numpad
 #define GAME 3 // Gamepad
+#define GAMEFN 4 // Gamepad Fn
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(FNKY)                // FN1 - Momentary Layer 1 (Fn and media)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergodox(
 		 // Left hand
-		 KC_GRV,  KC_1,    KC_2,    KC_3,  KC_4,    KC_5,          KC_ESC,
-		 KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,          KC_BSLS,
-		 KC_CAPS, KC_A,    KC_S,    KC_D,  KC_F,    KC_G,
-		 KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,          GUI_T(KC_LGUI),
-		 MO(1),   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
+		 KC_GRV,   KC_1,    KC_2,    KC_3,  KC_4,    KC_5,          KC_ESC,
+		 KC_TAB,   KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,          KC_BSLS,
+		 KC_CAPS,  KC_A,    KC_S,    KC_D,  KC_F,    KC_G,
+		 KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,          GUI_T(KC_LGUI),
+		 MO(FNKY), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,
 		                                            CTL_T(KC_NO), ALT_T(KC_APP),
 		                                                                KC_HOME,
 		                                               KC_BSPC, KC_DEL,  KC_END,
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		     KC_LBRC,       KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RBRC,
 		                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
 		     RGUI_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-		                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TG(2),
+		                             KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, TG(MSNM),
 		 RALT_T(KC_NO), RCTL_T(KC_NO),
 		 KC_PGUP,
 		 KC_PGDN, KC_ENT, KC_SPC
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
 		              KC_MPLY, KC_MSTP, KC_MPRV, KC_MNXT, KC_NO,  KC_TRNS,
 		     KC_TRNS, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_NO,  KC_TRNS,
-		                       KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,  TG(3),
+		                       KC_PSCR, KC_SLCK, KC_PAUS, KC_NO,  TG(GAME),
 		 KC_TRNS, KC_TRNS,
 		 KC_TRNS,
 		 KC_TRNS, KC_TRNS, KC_TRNS
@@ -79,6 +79,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [GAME] = LAYOUT_ergodox(
+		 KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		 KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		 KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		 KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		 TG(GAMEFN), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		                                              KC_TRNS, KC_TRNS,
+		                                                       KC_TRNS,
+		                                     KC_SPC, KC_BSPC,  KC_TRNS,
+		     EPRM,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(GAME),
+		 KC_TRNS, KC_TRNS,
+		 KC_TRNS,
+		 KC_TRNS, KC_TRNS, KC_DEL
+  ),
+
+  [GAMEFN] = LAYOUT_ergodox(
+		 // Left hand
 		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -86,12 +106,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		                                              KC_TRNS, KC_TRNS,
 		                                                       KC_TRNS,
-		                                     KC_TRNS, KC_SPC,  KC_TRNS,
-		     EPRM,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+		                                     KC_TRNS, KC_INS,  KC_TRNS,
+		 // Right hand
+		     KC_TRNS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,
+		     KC_TRNS, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
 		              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 		     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-		                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TG(GAME),
 		 KC_TRNS, KC_TRNS,
 		 KC_TRNS,
 		 KC_TRNS, KC_TRNS, KC_TRNS
