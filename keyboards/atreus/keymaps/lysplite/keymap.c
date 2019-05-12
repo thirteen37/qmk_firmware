ff4edca227810a0ab30e331b5dbed74b26461d67
Yu-Xi Lim <limyuxi@gmail.com>
2019-05-12 23:01:56 +0800

    Switch from KEYMAP to LAYOUT

--- keyboards/atreus/keymaps/lysplite/keymap.c
+++ keyboards/atreus/keymaps/lysplite/keymap.c
@@ -1,4 +1,4 @@
-#include "atreus.h"
+#include QMK_KEYBOARD_H
 
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
@@ -9,7 +9,7 @@
 #define _LW 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[_QW] = KEYMAP( /* Qwerty */
+[_QW] = LAYOUT( /* Qwerty */
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *  %       ^     {     }    ~        ||       &     1     2     3    \
  * lower  insert  fn  shift bksp ctrl || alt space  super  .     0    =
  */
-[_RS] = KEYMAP( /* [> RAISE <] */
+[_RS] = LAYOUT( /* [> RAISE <] */
   KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_PIPE,                   KC_UP,   KC_7,    KC_8,   KC_9, KC_ASTR ,
   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,                    KC_DOWN, KC_4,    KC_5,   KC_6, KC_PLUS ,
   KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_TILD,                   KC_AMPR, KC_1,    KC_2,   KC_3, KC_BSLS ,
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *       volup             reset      ||             F1     F2    F3     F12
  *       voldn   L0  shift bksp  ctrl || alt space  super  prtsc scroll pause
  */
-[_LW] = KEYMAP( /* [> LOWER <] */
+[_LW] = LAYOUT( /* [> LOWER <] */
   KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
   KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_DOWN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
   KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
