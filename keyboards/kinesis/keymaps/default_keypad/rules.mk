eb1d7bfc5577b70dbbab7132e1bd48d6a7f6a597
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-08 23:53:26 +0800

    Fix locking interaction with Bootmagic

--- keyboards/kinesis/keymaps/default_keypad/rules.mk
+++ keyboards/kinesis/keymaps/default_keypad/rules.mk
@@ -1,4 +1,3 @@
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-AUDIO_ENABLE = yes
+UNICODE_ENABLE = no         # Unicode
