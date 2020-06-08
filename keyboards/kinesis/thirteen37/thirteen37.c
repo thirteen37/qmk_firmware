ae00810f993a0b5b25860de850f5157da688559d
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-08 23:41:45 +0800

    Clearly separate out LED handling logic

--- keyboards/kinesis/thirteen37/thirteen37.c
+++ keyboards/kinesis/thirteen37/thirteen37.c
@@ -2,31 +2,40 @@
 
 bool led_update_kb(led_t led_state) {
     if (led_update_user(led_state)) {
-        writePin(B12, !led_state.caps_lock);
-        writePin(B13, !led_state.num_lock);
-        writePin(B14, !led_state.scroll_lock);
+        if (led_state.caps_lock) caps_lock_led_on(); else caps_lock_led_off();
+        if (led_state.num_lock) num_lock_led_on(); else num_lock_led_off();
+        if (led_state.scroll_lock) scroll_lock_led_on(); else scroll_lock_led_off();
+        return true;
+    } else {
+        return false;
     }
-    return true;
 }
 
-void keyboard_post_init_kb(void) {
+void keyboard_pre_init_kb(void) {
     /* Setup LEDs */
     setPinOutput(B12);
     setPinOutput(B13);
     setPinOutput(B14);
     setPinOutput(B15);
-    writePinLow(B12);
+}
+
+void keyboard_post_init_kb(void) {
+    /* Animate LEDs */
+    caps_lock_led_on();
     wait_ms(50);
-    writePinLow(B13);
+    num_lock_led_on();
     wait_ms(50);
-    writePinHigh(B12);
-    writePinLow(B14);
+    caps_lock_led_off();
+    scroll_lock_led_on();
     wait_ms(50);
-    writePinHigh(B13);
-    writePinLow(B15);
+    num_lock_led_off();
+    keypad_led_on();
     wait_ms(50);
-    writePinHigh(B14);
+    scroll_lock_led_off();
     wait_ms(50);
+    keypad_led_off();
+}
+
 /* LED helpers */
 
 void all_led_off(void) {
