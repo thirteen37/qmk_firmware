ed7f5edce1683e84a6a6749589fdec36ce871437
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-08 23:41:08 +0800

    LED helper functions

--- keyboards/kinesis/kinesis.h
+++ keyboards/kinesis/kinesis.h
@@ -27,3 +27,7 @@ void num_lock_led_on(void);
 void caps_lock_led_on(void);
 void scroll_lock_led_on(void);
 void keypad_led_on(void);
+void num_lock_led_off(void);
+void caps_lock_led_off(void);
+void scroll_lock_led_off(void);
+void keypad_led_off(void);
