bf542da0fe500acc47f539da9fae7f9a8f5d6602
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-08 17:37:06 +0800

    More comprehensive layout

---
+++ keyboards/kinesis/keymaps/default_keypad/readme.md
@@ -0,0 +1,5 @@
+# Complete default keymap for kinesis-advantage
+
+This keymap is a more comprehensive version of the one in the
+`default_pretty` directory, including the `keypad` and `program`
+layers.
---
+++ keyboards/kinesis/keymaps/default_keypad/rules.mk
@@ -0,0 +1,4 @@
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+COMMAND_ENABLE = yes        # Commands for debug and configuration
+AUDIO_ENABLE = yes
