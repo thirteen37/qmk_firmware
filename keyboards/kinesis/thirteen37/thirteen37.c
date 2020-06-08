f8043c7c696e28d71490164c3b4a2f0e7604d345
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-08 23:41:08 +0800

    LED helper functions

--- keyboards/kinesis/thirteen37/thirteen37.c
+++ keyboards/kinesis/thirteen37/thirteen37.c
@@ -27,5 +27,50 @@ void keyboard_post_init_kb(void) {
     wait_ms(50);
     writePinHigh(B14);
     wait_ms(50);
+/* LED helpers */
+
+void all_led_off(void) {
+    caps_lock_led_off();
+    num_lock_led_off();
+    scroll_lock_led_off();
+    keypad_led_off();
+}
+
+void all_led_on(void) {
+    caps_lock_led_on();
+    num_lock_led_on();
+    scroll_lock_led_on();
+    keypad_led_on();
+}
+
+void num_lock_led_on(void) {
+    writePinLow(B13);
+}
+
+void caps_lock_led_on(void) {
+    writePinLow(B12);
+}
+
+void scroll_lock_led_on(void) {
+    writePinLow(B14);
+}
+
+void keypad_led_on(void) {
+    writePinLow(B15);
+}
+
+void num_lock_led_off(void) {
+    writePinHigh(B13);
+}
+
+void caps_lock_led_off(void) {
+    writePinHigh(B12);
+}
+
+void scroll_lock_led_off(void) {
+    writePinHigh(B14);
+}
+
+void keypad_led_off(void) {
     writePinHigh(B15);
 }
