50d0d5330b1ab609af6d3d359094039bfbea1c95
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-09 00:07:54 +0800

    Persistent base layers
    Also with audio

--- keyboards/kinesis/keymaps/default_keypad/config.h
+++ keyboards/kinesis/keymaps/default_keypad/config.h
@@ -2,3 +2,9 @@
 
 // place overrides here
 #define AUDIO_CLICKY
+
+#define DEFAULT_LAYER_SONGS                      \
+    {                                            \
+     SONG(QWERTY_SOUND),                         \
+     SONG(DVORAK_SOUND)                          \
+    }
