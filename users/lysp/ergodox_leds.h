7496068cbfa529e5eeea8585fb6ef4b915eec3c6
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-13 18:13:03 +0800

    Make Atreus Lysp layout work with userspace

---
+++ users/lysp/ergodox_leds.h
@@ -0,0 +1,29 @@
+#pragma once
+
+/* Fills for Ergodox LED functions */
+
+#if !defined(KEYBOARD_ergodox_ez) && !defined(KEYBOARD_ergodox_infinity)
+void ergodox_board_led_on(void){
+}
+
+void ergodox_right_led_1_on(void){
+}
+
+void ergodox_right_led_2_on(void){
+}
+
+void ergodox_right_led_3_on(void){
+}
+
+void ergodox_board_led_off(void){
+}
+
+void ergodox_right_led_1_off(void){
+}
+
+void ergodox_right_led_2_off(void){
+}
+
+void ergodox_right_led_3_off(void){
+}
+#endif
