41e21fa2f49cdb41c7c35155e48e9580fb901489
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-13 19:01:00 +0800

    Add Lysp to Kinesis

---
+++ keyboards/kinesis/keymaps/lysp/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+// place overrides here
+#define AUDIO_CLICKY
+
+#define DEFAULT_LAYER_SONGS                      \
+    {                                            \
+     SONG(QWERTY_SOUND),                         \
+     SONG(DVORAK_SOUND),                         \
+     SONG(COLEMAK_SOUND),                        \
+     SONG(WORKMAN_SOUND),                        \
+    }
