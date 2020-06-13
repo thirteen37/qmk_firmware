d8e665e2cc7d6c9b8f92e2656aea796f23711a20
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-13 17:46:39 +0800

    Refactor Ergodox lysp layout
    Move more stuff into userspace

--- users/lysp/lysp.c
+++ users/lysp/lysp.c
@@ -10,9 +10,6 @@
 #define GAME 3 // Gamepad
 #define GAMEFN 4 // Gamepad Fn
 
-const uint16_t PROGMEM fn_actions[] = {
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergodox(
@@ -118,3 +115,112 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		 KC_TRNS, KC_TRNS, KC_TRNS
   ),
 };
+
+const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
+{
+      switch(id) {
+        case 0:
+        if (record->event.pressed) {
+          SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+        }
+        break;
+      }
+    return MACRO_NONE;
+};
+
+const uint16_t PROGMEM fn_actions[] = {
+};
+
+void matrix_init_user(void) {
+#ifdef RGBLIGHT_COLOR_LAYER_0
+  rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+#endif
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    // dynamically generate these.
+    case VRSN:
+      if (record->event.pressed) {
+        SEND_STRING (QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+      }
+      return false;
+      break;
+    case RGB_SLD:
+      if (record->event.pressed) {
+#ifdef RGBLIGHT_ENABLE
+        rgblight_mode(1);
+#endif
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+uint32_t layer_state_set_user(uint32_t state) {
+
+    uint8_t layer = biton32(state);
+
+    ergodox_board_led_off();
+    ergodox_right_led_1_off();
+    ergodox_right_led_2_off();
+    ergodox_right_led_3_off();
+    switch (layer) {
+    case 0:
+#ifdef RGBLIGHT_COLOR_LAYER_0
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_0);
+        #endif
+        break;
+    case 1:
+        ergodox_right_led_1_on();
+#ifdef RGBLIGHT_COLOR_LAYER_1
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_1);
+#endif
+        break;
+    case 2:
+        ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_2
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_2);
+#endif
+        break;
+    case 3:
+        ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_3
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_3);
+#endif
+        break;
+    case 4:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+#ifdef RGBLIGHT_COLOR_LAYER_4
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_4);
+#endif
+        break;
+    case 5:
+        ergodox_right_led_1_on();
+        ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_5
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_5);
+#endif
+        break;
+    case 6:
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_6
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+#endif
+        break;
+    case 7:
+        ergodox_right_led_1_on();
+        ergodox_right_led_2_on();
+        ergodox_right_led_3_on();
+#ifdef RGBLIGHT_COLOR_LAYER_7
+        rgblight_setrgb(RGBLIGHT_COLOR_LAYER_6);
+#endif
+        break;
+    default:
+        break;
+    }
+    return state;
+}
