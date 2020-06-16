35afe079ba13bb50ffa9315c04f64be61c4ca241
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-16 15:14:15 +0800

    Refactor LED handling

--- keyboards/kinesis/thirteen37/thirteen37.c
+++ keyboards/kinesis/thirteen37/thirteen37.c
@@ -1,4 +1,5 @@
 #include "thirteen37.h"
+#include "leds.h"
 
 #ifdef AUDIO_ENABLE
 float caps_on_song[][2] = SONG(CAPS_LOCK_ON_SOUND);
@@ -28,9 +29,9 @@ bool led_update_kb(led_t led_state) {
     }
 #endif
     if (led_update_user(led_state)) {
-        if (led_state.caps_lock) caps_lock_led_on(); else caps_lock_led_off();
-        if (led_state.num_lock) num_lock_led_on(); else num_lock_led_off();
-        if (led_state.scroll_lock) scroll_lock_led_on(); else scroll_lock_led_off();
+        caps_lock_led(!led_state.caps_lock);
+        num_lock_led(!led_state.num_lock);
+        scroll_lock_led(!led_state.scroll_lock);
         return true;
     } else {
         return false;
@@ -38,12 +39,7 @@ bool led_update_kb(led_t led_state) {
 }
 
 void keyboard_pre_init_kb(void) {
-    /* Setup LEDs */
-    setPinOutput(B12);
-    setPinOutput(B13);
-    setPinOutput(B14);
-    setPinOutput(B15);
-
+    init_leds();
     keyboard_pre_init_user();
 }
 
@@ -64,52 +60,4 @@ void keyboard_post_init_kb(void) {
     keypad_led_off();
 
     keyboard_post_init_user();
-}
-
-/* LED helpers */
-
-void all_led_off(void) {
-    caps_lock_led_off();
-    num_lock_led_off();
-    scroll_lock_led_off();
-    keypad_led_off();
-}
-
-void all_led_on(void) {
-    caps_lock_led_on();
-    num_lock_led_on();
-    scroll_lock_led_on();
-    keypad_led_on();
-}
-
-void num_lock_led_on(void) {
-    writePinLow(B13);
-}
-
-void caps_lock_led_on(void) {
-    writePinLow(B12);
-}
-
-void scroll_lock_led_on(void) {
-    writePinLow(B14);
-}
-
-void keypad_led_on(void) {
-    writePinLow(B15);
-}
-
-void num_lock_led_off(void) {
-    writePinHigh(B13);
-}
-
-void caps_lock_led_off(void) {
-    writePinHigh(B12);
-}
-
-void scroll_lock_led_off(void) {
-    writePinHigh(B14);
-}
-
-void keypad_led_off(void) {
-    writePinHigh(B15);
 }
