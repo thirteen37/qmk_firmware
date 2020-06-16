b8bcbc1d531a2f5af1591b5417757d57e41aa2b8
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-16 15:52:58 +0800

    Move keypad handling up to board level
    Previously logic resided in keymap

---
+++ keyboards/kinesis/thirteen37/sounds.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define KEYPAD_ON_SOUND E__NOTE(_A4), E__NOTE(_B4),
+#define KEYPAD_OFF_SOUND E__NOTE(_B4), E__NOTE(_A4),
--- users/lysp/lysp_audio.h
+++ users/lysp/lysp_audio.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#define KEYPAD_ON_SOUND E__NOTE(_A4), E__NOTE(_B4),
-#define KEYPAD_OFF_SOUND E__NOTE(_B4), E__NOTE(_A4),
-
 #define THUMB_WIN_SOUND E__NOTE(_GS5), E__NOTE(_A5), S__NOTE(_REST), E__NOTE(_E6), E__NOTE(_D6),
 #define THUMB_MAC_SOUND E__NOTE(_GS5), E__NOTE(_A5), S__NOTE(_REST), E__NOTE(_GS5), E__NOTE(_A6),
 #define THUMB_PC_SOUND E__NOTE(_GS5), E__NOTE(_A5), S__NOTE(_REST), Q__NOTE(_D6),
