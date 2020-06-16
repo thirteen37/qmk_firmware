e93a948c4a4c07eec3a50365ac0648b90cfa84f6
Yu-Xi Lim <limyuxi@gmail.com>
2020-06-16 15:14:15 +0800

    Refactor LED handling

---
+++ keyboards/kinesis/thirteen37/leds.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void init_leds(void);
+void all_led_off(void);
+void all_led_on(void);
+void caps_lock_led(int level);
+void num_lock_led(int level);
+void scroll_lock_led(int level);
+void keypad_led(int level);
+void caps_lock_led_on(void);
+void num_lock_led_on(void);
+void scroll_lock_led_on(void);
+void keypad_led_on(void);
+void caps_lock_led_off(void);
+void num_lock_led_off(void);
+void scroll_lock_led_off(void);
+void keypad_led_off(void);
