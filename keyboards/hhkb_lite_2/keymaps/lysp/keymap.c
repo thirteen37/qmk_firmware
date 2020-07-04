b7ce2d3ec7b879948b3eab86d37c4e847253efa3
Yu-Xi Lim <limyuxi@gmail.com>
2020-07-05 00:44:21 +0800

    Fix Lysp function layout

--- keyboards/hhkb_lite_2/keymaps/lysp/keymap.c
+++ keyboards/hhkb_lite_2/keymaps/lysp/keymap.c
@@ -26,9 +26,9 @@ LAYOUT(
 LAYOUT(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_ESC,
     KC_TAB,      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,      KC_BSPC,
-    KC_LGUI,         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
+    GUI_T(KC_ESC),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,           KC_ENT,
     KC_LSFT,             KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,                KC_RSFT,
-                 MO(_FN), KC_LALT, KC_LCTL,                       KC_SPC,                        KC_RCTL, KC_RALT,
+                 MO(_FL), KC_LALT, KC_LCTL,                       KC_SPC,                        KC_RCTL, KC_RALT,
                                                                                                                          KC_UP,
                                                                                                                 KC_LEFT, KC_DOWN, KC_RGHT
 ),
@@ -44,13 +44,13 @@ LAYOUT(
 ),
 [_FL] =
 LAYOUT(
-    KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,
-    _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   _______,    _______,
-    _______,         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,           _______,
-    _______,             NORM,    _______, _______, _______, _______, _______, _______, _______, _______, _______,                _______,
-                 _______, _______, _______,                        _______,                   _______, _______,
-                                                                                                                         _______,
-                                                                                                                _______, _______, _______
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  RESET,
+    KC_CAPS,     _______, _______, _______, _______, _______, _______, KC_MUTE, KC_PSCR, KC_BRMD, KC_BRMU, KC_UP,   XXXXXXX,      KC_BSPC,
+    _______,         _______, _______, _______, _______, _______, _______, KC_VOLU, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT,          _______,
+    _______,             NORM,    _______, _______, _______, _______, _______, KC_VOLD, KC_END, KC_PGDN, KC_DOWN,                 _______,
+                 _______, _______, _______,                        TO(_MN),                     _______, _______,
+                                                                                                                         KC_PGUP,
+                                                                                                                KC_HOME, KC_PGDN, KC_END
 ),
 [_MN] =
 LAYOUT(
