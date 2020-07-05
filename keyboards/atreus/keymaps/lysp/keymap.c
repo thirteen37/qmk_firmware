d99b63474e5d44d4557d169cdc890eb98a6e7ff2
Yu-Xi Lim <limyuxi@gmail.com>
2020-07-05 23:28:05 +0800

    Clean up Atreus Lysp

--- keyboards/atreus/keymaps/lysp/keymap.c
+++ keyboards/atreus/keymaps/lysp/keymap.c
@@ -12,10 +12,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT( /* Qwerty */
-  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
-  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
-  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-  KC_ESC, KC_TAB, MO(_RS),  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, KC_SPC,  KC_LGUI, KC_MINS, KC_QUOT, KC_ENT  ),
+  KC_Q,           KC_W,   KC_E,     KC_R,    KC_T,                       KC_Y,           KC_U,    KC_I,    KC_O,    KC_P    ,
+  KC_A,           KC_S,   KC_D,     KC_F,    KC_G,                       KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,           KC_X,   KC_C,     KC_V,    KC_B,                       KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  LT(_SD,KC_ESC), KC_TAB, MO(_RS),  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, LT(_MK,KC_SPC), KC_LGUI, KC_MINS, KC_QUOT, KC_SFTENT ),
 /*
  *  !       @     [     ]    `        ||      up     7     8     9    *
  *  #       $     (     )    |        ||      dn     4     5     6    +
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_GRV,                    KC_UP,   KC_7,    KC_8,   KC_9, KC_ASTR ,
   KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_PIPE,                   KC_DOWN, KC_4,    KC_5,   KC_6, KC_PLUS ,
   KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_EQL,                    KC_AMPR, KC_1,    KC_2,   KC_3, KC_BSLS ,
-  TG(_LW), KC_INS,  KC_TRNS, KC_TRNS, KC_DELT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DOT, KC_0, KC_TRNS ),
+  TG(_LW), KC_INS,  _______, _______, KC_DELT, _______, _______, _______, _______, KC_DOT, KC_0, _______ ),
 /*
  * insert home   up  end   pgup       ||      up     F7     F8    F9     F10
  *  del   left  down right pgdn       ||     down    F4     F5    F6     F11
@@ -36,56 +36,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LW] = LAYOUT( /* [> LOWER <] */
   KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
   KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-  KC_VOLD, KC_VOLU, KC_MUTE, KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-  KC_NO,   KC_CAPS, TO(_QW), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR, KC_SLCK, KC_PAUS ),
+  KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX, RESET,                     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+  XXXXXXX, KC_CAPS, TO(_QW), _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS ),
 /* Super Duper */
 [_SD] = LAYOUT( /* [> Super Duper <] */
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS ,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS ,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS ,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS ),
+  _______, _______, _______, _______, _______,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END,   _______ ,
+  _______, _______, _______, _______, _______,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______ ,
+  _______, _______, _______, _______, _______,                   _______, _______, _______, _______,  _______ ,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______ ),
 /* Mouse keys */
 [_MK] = LAYOUT( /* [> Mouse keys <] */
-  KC_BTN3, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-  KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ),
+  KC_BTN3, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U,                   _______, _______, _______, _______, _______ ,
+  _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                   _______, _______, _______, _______, _______ ,
+  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______ ,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ ),
 
 };
-
-
-// Super Duper
-
-enum combos {
-  SUPERDUPER,
-  MK,
-};
-
-const uint16_t PROGMEM sd_combo[] = {KC_ESC, KC_TAB, COMBO_END};
-const uint16_t PROGMEM mk_combo[] = {KC_QUOT, KC_ENT, COMBO_END};
-
-combo_t key_combos[COMBO_COUNT] = {
-  [SUPERDUPER] = COMBO_ACTION(sd_combo),
-  [MK] = COMBO_ACTION(mk_combo),
-};
-
-
-void process_combo_event(uint8_t combo_index, bool pressed) {
-  switch(combo_index) {
-  case SUPERDUPER:
-    if (pressed) {
-      layer_on(_SD);
-    } else {
-      layer_off(_SD);
-      unregister_mods(MOD_BIT(KC_LGUI) | MOD_BIT(KC_LCTL) | MOD_BIT(KC_LALT)); // Sometimes mods are held, unregister them
-    }
-    break;
-  case MK:
-    if (pressed) {
-      layer_on(_MK);
-    } else {
-      layer_off(_MK);
-    }
-    break;
-  }
-}
