71dc72e2e802ae6bf3315d2e0627cd83355c16e8
Yu-Xi Lim <limyuxi@gmail.com>
2020-07-06 00:44:09 +0800

    Double-tap for CAPS

--- keyboards/atreus/keymaps/lysp/keymap.c
+++ keyboards/atreus/keymaps/lysp/keymap.c
@@ -1,21 +1,25 @@
 #include QMK_KEYBOARD_H
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
 #define _QW 0
 #define _RS 1
 #define _LW 2
 #define _SD 3
 #define _MK 4
 
+enum {
+    TD_SFT_CAPS,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_SFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT( /* Qwerty */
-  KC_Q,           KC_W,   KC_E,     KC_R,    KC_T,                       KC_Y,           KC_U,    KC_I,    KC_O,    KC_P    ,
-  KC_A,           KC_S,   KC_D,     KC_F,    KC_G,                       KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN ,
-  KC_Z,           KC_X,   KC_C,     KC_V,    KC_B,                       KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-  LT(_SD,KC_ESC), KC_TAB, MO(_RS),  KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, LT(_MK,KC_SPC), KC_LGUI, KC_MINS, KC_QUOT, KC_SFTENT ),
+  KC_Q,           KC_W,   KC_E,     KC_R,            KC_T,                       KC_Y,           KC_U,    KC_I,    KC_O,    KC_P    ,
+  KC_A,           KC_S,   KC_D,     KC_F,            KC_G,                       KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_Z,           KC_X,   KC_C,     KC_V,            KC_B,                       KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
+  LT(_SD,KC_ESC), KC_TAB, MO(_RS),  TD(TD_SFT_CAPS), KC_BSPC,  KC_LCTL, KC_LALT, LT(_MK,KC_SPC), KC_LGUI, KC_MINS, KC_QUOT, KC_SFTENT ),
 /*
  *  !       @     [     ]    `        ||      up     7     8     9    *
  *  #       $     (     )    |        ||      dn     4     5     6    +
