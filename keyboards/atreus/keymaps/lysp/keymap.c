0928b5be34d7a5fdc291d3db2e0274d0649df43d
Yu-Xi Lim <limyuxi@gmail.com>
2021-08-01 15:47:13 +0800

    Line up Atreus Lysp layout with Keyboardio

--- keyboards/atreus/keymaps/lysp/keymap.c
+++ keyboards/atreus/keymaps/lysp/keymap.c
@@ -21,26 +21,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Z,           KC_X,   KC_C,     KC_V,            KC_B,                       KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
   LT(_SD,KC_ESC), KC_TAB, MO(_RS),  TD(TD_SFT_CAPS), KC_BSPC,  KC_LCTL, KC_LALT, LT(_MK,KC_SPC), KC_LGUI, KC_MINS, KC_QUOT, KC_SFTENT ),
 /*
- *  !       @     [     ]    `        ||      up     7     8     9    *
- *  #       $     (     )    |        ||      dn     4     5     6    +
- *  %       ^     {     }    =        ||       &     1     2     3    \
+ *  !       @     [     ]    &        ||      up     7     8     9    *
+ *  #       $     (     )    *        ||      dn     4     5     6    +
+ *  %       ^     {     }    =        ||       `     1     2     3    \
  * lower  insert  fn  shift  del ctrl || alt space  super  .     0    ⏎
  */
 [_RS] = LAYOUT( /* [> RAISE <] */
-  KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_GRV,                    KC_UP,   KC_7,    KC_8,   KC_9, KC_ASTR ,
-  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_PIPE,                   KC_DOWN, KC_4,    KC_5,   KC_6, KC_PLUS ,
-  KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_EQL,                    KC_AMPR, KC_1,    KC_2,   KC_3, KC_BSLS ,
+  KC_EXLM, KC_AT,   KC_LBRC, KC_RBRC, KC_AMPR,                   KC_UP,   KC_7,    KC_8,   KC_9, KC_ASTR ,
+  KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_ASTR,                   KC_DOWN, KC_4,    KC_5,   KC_6, KC_PLUS ,
+  KC_PERC, KC_CIRC, KC_LCBR, KC_RCBR, KC_EQL,                    KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
   TG(_LW), KC_INS,  _______, _______, KC_DELT, _______, _______, _______, _______, KC_DOT, KC_0, _______ ),
 /*
- * insert home   up  end   pgup       ||      up     F7     F8    F9     F10
- *  del   left  down right pgdn       ||     down    F4     F5    F6     F11
+ * insert home   ↑   end   pgup       ||      up     F7     F8    F9     F10
+ *  del    ←     ↓    →    pgdn       ||     down    F4     F5    F6     F11
  * voldn volup  mute       reset      ||             F1     F2    F3     F12
- *        caps   L0  shift bksp  ctrl || alt space  super  prtsc scroll pause
+ *        caps   L0  shift   ␈   ctrl || alt space  super  prtsc scroll pause
  */
 [_LW] = LAYOUT( /* [> LOWER <] */
-  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
-  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
-  KC_VOLD, KC_VOLU, KC_MUTE, XXXXXXX, RESET,                     XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
+  KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_VOLU, KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
+  KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_VOLD, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
+  KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, RESET,                     KC_MUTE, KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
   XXXXXXX, KC_CAPS, TO(_QW), _______, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS ),
 /* Super Duper */
 [_SD] = LAYOUT( /* [> Super Duper <] */
