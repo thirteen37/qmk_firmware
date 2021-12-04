c29c407ae1d045090ba330d021edec89725a6166
Yu-Xi Lim <limyuxi@gmail.com>
2021-12-04 21:59:02 +0800

    Remove bootmagic from kinesis
    Largely deprecated

--- keyboards/kinesis/thirteen37/rules.mk
+++ keyboards/kinesis/thirteen37/rules.mk
@@ -12,7 +12,6 @@ EXTRA_FLAGS =
 OPT_DEFS += -DCORTEX_ENABLE_WFI_IDLE=TRUE
 
 # This board has issues with the "full" Bootmagic
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 AUDIO_ENABLE = yes          # Audio output on port C6
