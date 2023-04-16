8856c0ca45cf1cf05a83f496ab62488009b150d4
Yu-Xi Lim <limyuxi@gmail.com>
2023-04-17 00:36:56 +0800

    Use caps word

--- keyboards/kinesis/keymaps/lysp/rules.mk
+++ keyboards/kinesis/keymaps/lysp/rules.mk
@@ -2,3 +2,5 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 UNICODE_ENABLE = no         # Unicode
 DYNAMIC_MACRO_ENABLE = yes
+COMMAND_ENABLE = no         # conflicts with CAPS_WORD_ENABLE
+CAPS_WORD_ENABLE = yes
