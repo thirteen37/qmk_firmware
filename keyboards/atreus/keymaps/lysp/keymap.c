c7caff02a369e6dcdf10d0809b686d6024f438e8
Yu-Xi Lim <limyuxi@gmail.com>
2023-07-23 18:01:13 +0800

    Tap dance Hyper key

--- keyboards/atreus/keymaps/lysp/keymap.c
+++ keyboards/atreus/keymaps/lysp/keymap.c
@@ -6,10 +6,21 @@
 #define _SD 3
 #define _MK 4
 
+// Tap Dance declarations
+enum {
+    TD_SCLN,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for semi-colon, twice for F19 (for use with Hyper key)
+    [TD_SCLN] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_F19),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QW] = LAYOUT( /* Qwerty */
   KC_Q,           KC_W,   KC_E,    KC_R,    KC_T,                       KC_Y,           KC_U,    KC_I,    KC_O,    KC_P    ,
-  KC_A,           KC_S,   KC_D,    KC_F,    KC_G,                       KC_H,           KC_J,    KC_K,    KC_L,    KC_SCLN ,
+  KC_A,           KC_S,   KC_D,    KC_F,    KC_G,                       KC_H,           KC_J,    KC_K,    KC_L,    TD(TD_SCLN),
   KC_Z,           KC_X,   KC_C,    KC_V,    KC_B,                       KC_N,           KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
   LT(_SD,KC_ESC), KC_TAB, MO(_RS), KC_LSFT, KC_BSPC,  KC_LCTL, KC_LALT, LT(_MK,KC_SPC), KC_LGUI, KC_MINS, KC_QUOT, SFT_T(KC_ENT) ),
 /*
