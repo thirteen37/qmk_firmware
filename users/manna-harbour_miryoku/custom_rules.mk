4af0743ca160bad15c297bd40de878883390aabf
Yu-Xi Lim <limyuxi@gmail.com>
2023-08-12 18:12:59 +0800

    Experiement with Miryoku for smaller kbs

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,8 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+MIRYOKU_CLIPBOARD=MAC
+MIRYOKU_EXTRA=COLEMAKDH
+MIRYOKU_NAV=VI
+MIRYOKU_ALPHAS=QWERTY
+MIRYOKU_TAP=QWERTY
